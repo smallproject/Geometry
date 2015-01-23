7a4e3baaf6fe78001bee3301a73644b0d9c707ba
smallproject <j.a.villas@live.com.ph>
2015-01-23 16:28:08 +0100

    small change 01

---
+++ source/Console_geometry/Console_geometry/Area.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_geometry
+{
+    class Area
+    {
+        public decimal square(decimal side)
+        {
+            return side*side;
+        }
+
+        public decimal triangle(decimal side)
+        {
+            return 0;
+        }
+    }
+
+    class perimeter
+    {
+        public decimal square(decimal side)
+        {
+            return side*4;
+        }
+
+        public decimal rectangle(decimal length, decimal width)
+        {
+            return (2*length) + (2*width);
+        }
+
+        public decimal triangle(decimal a, decimal b, decimal c)
+        {
+            return a + b + c;
+        }
+
+        public decimal quadrilateral(decimal a, decimal b, decimal c, decimal d)
+        {
+            return a + b + c + d;
+        }
+
+        public double circle(double radius)
+        {
+            return 2*Math.PI*radius;
+        }
+    }
+}
--- source/Console_geometry/Console_geometry/Program.cs
+++ source/Console_geometry/Console_geometry/Program.cs
@@ -10,6 +10,20 @@ namespace Console_geometry
     {
         static void Main(string[] args)
         {
+
+            int sizeofdecimal = sizeof (decimal);
+            int sizeofdouble = sizeof (double);
+            int sizeoffloat = sizeof (float);
+            int sizeofint = sizeof (int);
+            int sizeofint16 = sizeof(Int16);
+            int sizeofint32 = sizeof(Int32);
+
+            Console.WriteLine("decimal: {0}", sizeofdecimal.ToString());
+            Console.WriteLine("double: {0}", sizeofdouble.ToString());
+            Console.WriteLine("float: {0}", sizeoffloat.ToString());
+            Console.WriteLine("int: {0}", sizeofint.ToString());
+            Console.WriteLine("int16: {0}", sizeofint16.ToString());
+            Console.WriteLine("int32: {0}", sizeofint32.ToString());
         }
     }
 }
